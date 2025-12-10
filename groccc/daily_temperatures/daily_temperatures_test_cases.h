fca4638df2f9664c83552daf2e9544c07e406652
Alexander Lopez <agl.alexglopez@gmail.com>
2025-12-09 18:02:36 -0800

    add docs about input output on daily temps

--- groccc/daily_temperatures/daily_temperatures_test_cases.h
+++ groccc/daily_temperatures/daily_temperatures_test_cases.h
@@ -5,14 +5,32 @@
 
 #include "../test_case_generator.h"
 
+/** The input temperature buffer with convenience output solution buffer
+provided. Because the daily temperatures problem specifies the return value
+has an entry for each temperature, it is provided here. The result buffer
+will be provided with capacity equivalent to temperatures but 0 count.
+
+Note that the result buffer may be freely and trivially shallow copied and
+returned from a function. The underlying static memory to which it points
+does not move and metadata in the Buffer struct remains consistent. */
 struct Daily_temperatures_input
 {
+    /** The input buffer of N temperatures. */
     CCC_Buffer temperatures;
+    /** The return value for this problem of N entries representing the number
+    of days until a warmer temperature from the day at that index. Has
+    capacity equivalent to temperatures and initial count 0. May be shallow
+    copied into output return struct safely. Underlying storage does not
+    move. */
     CCC_Buffer days_until_warmer_temperature_result;
 };
 
+/** The correct output buffer of N entries corresponding to the temperature for
+each day. Each entry at index i represents the number of days required to see
+a warmer temperature than the temperature recorded at the input buffer. */
 struct Daily_temperatures_output
 {
+    /** The output buffer of correct days until warmer temperatures. */
     CCC_Buffer days_until_warmer_temperature;
 };
 
