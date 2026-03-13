e71d21bd0b9e3ea830516946f1c72180dcfc6142
Alex Lopez <agl.alexglopez@gmail.com>
2026-03-13 09:26:47 -0700

    add problem

---
+++ groccc/sort_an_array/sort_an_array.c
@@ -0,0 +1,68 @@
+#include <stdbool.h>
+
+#include "utility/loggers.h"
+#include "utility/test_case_generator.h"
+
+#define BUFFER_USING_NAMESPACE_CCC
+#define TRAITS_USING_NAMESPACE_CCC
+#define FLAT_PRIORITY_QUEUE_USING_NAMESPACE_CCC
+#include "ccc/buffer.h"
+#include "ccc/flat_priority_queue.h"
+#include "ccc/traits.h"
+#include "ccc/types.h"
+
+#include "sort_an_array_tests.h"
+
+static CCC_Order
+compare_ints(CCC_Type_comparator_context const context) {
+    int const *const lhs = context.type_left;
+    int const *const rhs = context.type_right;
+    return (*lhs > *rhs) - (*lhs < *rhs);
+}
+
+static inline bool
+buffers_match(Buffer const *const a, Buffer const *const b) {
+    if (count(a).count != count(b).count) {
+        return false;
+    }
+    for (int const *a_int = begin(a), *b_int = begin(b); a_int != end(a);
+         a_int = next(a, a_int), b_int = next(b, b_int)) {
+        if (*a_int != *b_int) {
+            return false;
+        }
+    }
+    return true;
+}
+
+/* TODO: solve sort_an_array. Optionally change function signature if desired.
+ */
+static struct Sort_an_array_output
+sort_an_array(struct Sort_an_array_input *input) {
+    if (is_empty(&input->ints)) {
+        return (struct Sort_an_array_output){};
+    }
+    Flat_priority_queue pq = flat_priority_queue_heapify(
+        int, CCC_ORDER_GREATER, compare_ints, NULL, NULL,
+        capacity(&input->ints).count, count(&input->ints).count,
+        begin(&input->ints));
+    return (struct Sort_an_array_output){
+        flat_priority_queue_heapsort(&pq, &(int){}),
+    };
+}
+
+int
+main(void) {
+    int passed = 0;
+    TCG_for_each_test_case(sort_an_array_tests, {
+        struct Sort_an_array_output const output
+            = sort_an_array(&TCG_test_case_input(sort_an_array_tests));
+        struct Sort_an_array_output const *const correct_output
+            = &TCG_test_case_output(sort_an_array_tests);
+        if (!buffers_match(&output.sorted_ints, &correct_output->sorted_ints)) {
+            logfail(sort_an_array_tests);
+        } else {
+            ++passed;
+        }
+    });
+    return TCG_tests_status(sort_an_array_tests, passed);
+}
---
+++ groccc/sort_an_array/sort_an_array_tests.h
@@ -0,0 +1,53 @@
+#ifndef SORT_AN_ARRAY_TESTS_H
+#define SORT_AN_ARRAY_TESTS_H
+
+#include "utility/test_case_generator.h"
+
+#include "ccc/buffer.h"
+
+struct Sort_an_array_input {
+    CCC_Buffer ints;
+};
+
+struct Sort_an_array_output {
+    CCC_Buffer sorted_ints;
+};
+
+TCG_tests_begin(sort_an_array_tests,
+                struct Sort_an_array_input,
+                struct Sort_an_array_output)
+
+TCG_test_case("empty", {
+    .input = {
+        .ints = {},
+    },
+    .output = {
+        .sorted_ints = {},
+    },
+})
+
+TCG_test_case("reverse order", {
+    .input = {
+        .ints = CCC_buffer_with_storage(5, (int[5]){2,1,0,-1,-2}),
+    },
+    .output = {
+        .sorted_ints = CCC_buffer_with_storage(5, (int[5]){-2,-1,0,1,2}),
+    },
+})
+
+TCG_test_case("random order", {
+    .input = {
+        .ints = CCC_buffer_with_storage(10,
+            (int[10]){8, 1, -34, 22, 4, 19, 12, 88, 93, 2}
+        ),
+    },
+    .output = {
+        .sorted_ints = CCC_buffer_with_storage(10,
+            (int[10]){-34, 1, 2, 4, 8, 12, 19, 22, 88, 93}
+        ),
+    },
+})
+
+TCG_tests_end(sort_an_array_tests);
+
+#endif /* SORT_AN_ARRAY_TESTS_H */
