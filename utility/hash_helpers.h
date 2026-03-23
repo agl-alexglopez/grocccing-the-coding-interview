4d0b359b560b3bf18042ef209cf8e6cefa0fe862
Alex Lopez <agl.alexglopez@gmail.com>
2026-03-23 13:39:22 -0700

    fix comment

--- utility/hash_helpers.h
+++ utility/hash_helpers.h
@@ -18,7 +18,8 @@ CCC_Order hash_map_int_key_val_order(CCC_Key_comparator_arguments arguments);
 uint64_t hash_map_int_to_u64(CCC_Key_arguments arguments);
 
 /** @brief A utility hasher for arbitrary hashing and safe aliasing.
-@param[in] str key context for which the key field is a pointer to SV_Str_view.
+@param[in] string_arguments key argument for which the key field is a pointer to
+SV_Str_view.
 @return the 64 bit hash value for the input bytes.
 @note Consider using this for strings or aliasing any data of variable length
 and type.
@@ -28,9 +29,12 @@ correct length for the SV_Str_view.
 
 It is safe to alias any data with this function. The C language specifies that
 any type may be safely aliased by `char *` and `char const *`. This hash
-function also only iterates over the range provided so it is safe to provide
-non-null terminated data to this function (even though the SV_Str_view library
-says this is not recommended). */
+function also only iterates read-only over the range provided so it is safe to
+provide non-null terminated data to this function (even though the SV_Str_view
+library says this is not recommended). Just ensure the SV_Str_view is the
+correct length when provided. SV_Str_view assumes that the position at the
+length of the input string may or may not be the null terminator and this
+function does not use any methods that need or care what is at that position. */
 uint64_t hash_fnv_1a_str_view_to_u64(CCC_Key_arguments string_arguments);
 
 #endif /* HASH_HELPERS_H */
