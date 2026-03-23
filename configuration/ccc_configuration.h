5d09f205ccfa0e356e25fc7af840ab00cb4a5282
Alexander Lopez <agl.alexglopez@gmail.com>
2026-03-22 18:25:38 -0700

    try freestand

---
+++ configuration/ccc_configuration.h
@@ -0,0 +1,7 @@
+#ifndef CCC_CONFIGURATION_H
+#define CCC_CONFIGURATION_H
+
+#include <assert.h> /* IWYU pragma: export */
+#include <string.h> /* IWYU pragma: export */
+
+#endif /* CCC_CONFIGURATION_H */
