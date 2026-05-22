c2e44f911553cad1941c0ff8f35e095dc14688dd
Alexander Lopez <agl.alexglopez@gmail.com>
2026-05-22 14:59:43 -0700

    add alignment checks to groccc problems

--- utility/allocators.c
+++ utility/allocators.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stddef.h>
 #include <stdlib.h>
 
@@ -5,8 +6,23 @@
 
 #include "ccc/types.h"
 
+#include "ccc/flat_hash_map.h" /* IWYU pragma: keep */
+static_assert(
+    alignof(max_align_t) >= CCC_FLAT_HASH_MAP_GROUP_COUNT,
+    "Ensure stdlib malloc/realloc/free can align SIMD group loads for flat "
+    "hash map."
+);
+
 void *
 stdlib_allocate(CCC_Allocator_arguments const arguments) {
+    if (arguments.alignment && arguments.alignment > alignof(max_align_t)) {
+        assert(
+            arguments.alignment <= alignof(max_align_t)
+            && "Any type provided to this allocator has alignment less than or "
+               "equal to default malloc/realloc max alignment."
+        );
+        return NULL;
+    }
     if (!arguments.input && !arguments.bytes) {
         return NULL;
     }
